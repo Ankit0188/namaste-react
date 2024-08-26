85a949a16f9db0cba246c2f538dde7b9723f9f54
Ankit <ankitpoddars007@gmail.com>
2024-08-26 19:32:25 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child1" },
+      [React.createElement("h1", {}, "I'm H1 Tag,In Javascript from child1"),
+        React.createElement("h2", {}, "I'm H2 Tag,In Javascript")]
+    ),
+    React.createElement(
+        "div",
+        { id: "child2" },
+        [React.createElement("h1", {}, "I'm H1 Tag,In Javascript from child2"),
+          React.createElement("h2", {}, "I'm H2 Tag,In Javascript")]
+      )
+  );
+  console.log(parent); //object
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+
